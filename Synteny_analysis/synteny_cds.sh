5de166856d64bd8c0f5750fd9b702196ffe2adf7
Yi-HuangUCR <81652574+Yi-HuangUCR@users.noreply.github.com>
2021-09-23 13:11:05 -0400

    Delete MCScan scripts

--- Synteny_analysis/synteny_cds.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -l
-python -m jcvi.compara.catalog ortholog ag ac --cscore=.99 --no_strip_names
-python -m jcvi.graphics.dotplot ag.ac.anchors
-
--- Synteny_analysis/synteny_prot.sh
+++
@@ -1,4 +0,0 @@
-!/bin/bash -l
-python -m jcvi.compara.catalog ortholog ag ac --cscore=.99 --no_strip_names --dbtype prot
-python -m jcvi.graphics.dotplot ag.ac.anchors
-
